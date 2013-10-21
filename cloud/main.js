1fe70cf30ec09960f80e33b910a36c537de91e7c
Administrator <admin@local.host>
2013-10-21 11:48:40 +0800

    update`

--- cloud/main.js
+++ cloud/main.js
@@ -1,15 +1,6 @@
 // Use AV.Cloud.define to define as many cloud functions as you want.
 // For example:
 AV.Cloud.define("hello", function(request, response) {
-  var crypto = require('crypto');
-  var express = require('express');
-  var yaml = require('js-yaml');
-  var　str = "";
-  config = "test: !!js/function > \n  \
-  function f() { \n    \
-   str = 'HelloWorld' \n  \
-  }();"
-  
-  yaml.load(config);
-  response.success(str);
+
+  response.success("hello");
 });
